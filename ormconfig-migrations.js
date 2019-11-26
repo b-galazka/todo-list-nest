2ada7228a246c9f7644383f1d77a36b5e3e85378
b-galazka <bartlomiejgalazka@zoho.eu>
2019-11-26 20:47:03 +0100

    improve migrations config

--- ormconfig-migrations.js
+++ ormconfig-migrations.js
@@ -2,6 +2,7 @@ const ormConfig = require('./ormconfig');
 
 module.exports = {
   ...ormConfig,
+  entities: ['./**/*.entity.ts'],
   migrations: ['migrations/*.ts'],
   cli: {
     migrationsDir: 'migrations'
