cda27326c3795680e1120f36bf07643e91efe471
b-galazka <bartlomiejgalazka@zoho.eu>
2019-11-29 20:56:31 +0100

    add circle ci

--- src/modules/tasks/tasks.controller.spec.ts
+++ src/modules/tasks/tasks.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 describe('Tasks Controller', () => {
   let controller: TasksController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController]
+      controllers: [TasksController],
+      providers: [
+        {
+          provide: TasksService,
+          useValue: {}
+        }
+      ]
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
--- src/modules/tasks/tasks.service.spec.ts
+++ src/modules/tasks/tasks.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+import { TaskEntity } from './task.entity';
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
   let service: TasksService;
 
+  const tasksRepositoryMock = {
+    metadata: {
+      columns: [] as any[],
+      relations: [] as any[]
+    }
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService]
+      providers: [
+        TasksService,
+        { provide: getRepositoryToken(TaskEntity), useValue: tasksRepositoryMock }
+      ]
     }).compile();
 
     service = module.get<TasksService>(TasksService);
