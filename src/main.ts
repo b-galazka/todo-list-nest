bac6b10e00b395aee06cb4d36f4fb896e092b808
b-galazka <bartlomiejgalazka@zoho.eu>
2019-12-04 21:03:56 +0100

    add log on application startup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { environment } from './environment';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap(logger: Logger): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: environment.allowedDomains });
 
   await app.listen(environment.port, environment.ip);
+
+  logger.log(`Listening at ${environment.ip}:${environment.port}`);
 }
 
-bootstrap();
+bootstrap(new Logger('NestApplication'));
